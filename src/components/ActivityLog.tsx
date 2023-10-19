068b69c01fd8f25ccc5ec1d753a7b591104ff3ec
audrey <audreylandoy@gmail.com>
2023-10-19 15:04:58 -0700

    added activity log used transportation end point

---
+++ src/components/ActivityLog.tsx
@@ -0,0 +1,39 @@
+import { Navigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import axios from "axios";
+
+function ActivityLog() {
+
+  const [activityList, setActivityList] = useState([]);
+
+  // make a response to http://127.0.0.1:8000/
+  const getTransportationActivities = () => {
+    axios
+      .get(`http://127.0.0.1:8000/api/eco-transport`)
+      .then(res => {
+        console.log(res.data)
+        setActivityList(res.data)
+      })
+  }
+
+  const activityElements = activityList.map((act) => {
+    return (
+      <li>
+        <ul>{act.activity_date}: {act.activity} {act.distance} miles</ul>
+      </li>
+    );
+  });
+
+  useEffect(() => {
+    getTransportationActivities();
+  }, []);
+
+  return (
+    <>
+      <h3>Activity Log</h3>
+      {activityElements}
+    </>
+  );
+}
+
+export default ActivityLog;
--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,9 +1,11 @@
 import { Navigate } from "react-router-dom";
+import ActivityLog from "../components/ActivityLog";
 
 function Explore() {
   return (
     <>
       <h1>Choose Your Quest for Today</h1>
+      <ActivityLog/>
     </>
   );
 }
